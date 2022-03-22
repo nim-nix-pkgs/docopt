9e41e07977e830584fa9c5d3bb5cb921148e75b3
hugosenari <hugosenari@gmail.com>
2022-03-21 23:38:02 -0300

    chore: re-index ../nimpkgs/d/docopt/v0_6_1

--- v0_6_1/flake.nix
+++ v0_6_1/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-docopt-v0_6_1.flake = false;
   inputs.src-docopt-v0_6_1.owner = "docopt";
-  inputs.src-docopt-v0_6_1.ref   = "refs/tags/v0.6.1";
+  inputs.src-docopt-v0_6_1.ref   = "v0_6_1";
   inputs.src-docopt-v0_6_1.repo  = "docopt.nim";
   inputs.src-docopt-v0_6_1.type  = "github";
   
